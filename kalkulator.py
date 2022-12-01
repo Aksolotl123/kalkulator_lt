70aeefac08146eb0a3d35fff08a1062133214e23
Aksolotl123 <tuzlukasz@gmail.com>
2022-12-01 02:26:26 +0100

    02:24

--- kalkulator.py
+++ kalkulator.py
@@ -51,14 +51,16 @@ def historia():
         for o in obliczenia:
             print(o)
 """rodzaje działań do wyboru"""
-rodzaje_dzialan = (
-    "Rodzaje działań: \n|||  1.Dodawanie  |||  2.Odejmowanie  \n|||  3.Mnożenie  |||  4.Dzielenie  \n|||  5.Potęgowanie "
-    "|||  6.Pierwiastek kwadratowy liczby\n|||  7.Logarytm naturalny  |||  8.Logarytm dziesiętny  \n|||  9.Sinus liczby  |||  10.Tangens liczby")
+slownik_rd = {1:"+",2:"-",3:"*",4:"/",5:"**",6:"√",7:"Log()",8:"Log10()",9:"Sin",10:"tg"}
 
 """właściwa część"""
 while True:
-    print(rodzaje_dzialan)
-    wybor_dzialania = input("Dokonaj wyboru działania (1-10): ")
+    print(
+    "Rodzaje działań: \n|||  1.Dodawanie  |||  2.Odejmowanie  \n|||  3.Mnożenie  |||  4.Dzielenie  \n|||  5.Potęgowanie "
+    "|||  6.Pierwiastek kwadratowy liczby\n|||  7.Logarytm naturalny  |||  8.Logarytm dziesiętny  \n|||  "
+    "9.Sinus liczby  |||  10.Tangens liczby  |||  11.Historia"
+    )
+    wybor_dzialania = input("Dokonaj wyboru działania (1-11): ")
     if wybor_dzialania in ("1", "2", "3", "4", "5"):
         while True:
             try:
@@ -73,25 +75,29 @@ while True:
             except ValueError:
                 print("Nie podano liczby!")
 
-        if wybor_dzialania == '1':
-            wynik =
-            print(liczba1, "+", liczba2, "=", dodawanie(liczba1, liczba2))
+        if wybor_dzialania == "1":
+            wynik =dodawanie(liczba1, liczba2)
+            print(liczba1, "+", liczba2, "=", wynik)
 
-        elif wybor_dzialania == '2':
+        elif wybor_dzialania == "2":
             wynik = odejmowanie(liczba1, liczba2)
-            print(liczba1, "-", liczba2, "=", odejmowanie(liczba1, liczba2))
+            print(liczba1, "-", liczba2, "=", wynik)
 
-        elif wybor_dzialania == '3':
+        elif wybor_dzialania == "3":
             wynik = mnozenie(liczba1, liczba2)
-            print(liczba1, "*", liczba2, "=", mnozenie(liczba1, liczba2))
+            print(liczba1, "*", liczba2, "=", wynik)
 
-        elif wybor_dzialania == '4':
+        elif wybor_dzialania == "4":
             wynik = dzielenie(liczba1, liczba2)
-            print(liczba1, "/", liczba2, "=", )
+            print(liczba1, "/", liczba2, "=", wynik)
 
-        elif wybor_dzialania == '5':
+        elif wybor_dzialania == "5":
             wynik = potegowanie(liczba1, liczba2)
-            print(liczba1, "do potęgi", liczba2, "=", )
+            print(liczba1, "do potęgi", liczba2, "=", wynik)
+
+        znaczek = slownik_rd[int(wybor_dzialania)]
+        ostatnie_obliczenie = f"{liczba1} {znaczek} {liczba2} = {wynik}"
+        historia_dzialan(ostatnie_obliczenie)
 
         nastepne_dzialanie = input("\nCzy chcesz wykonać następne działanie? (T/N): ").upper()
         if nastepne_dzialanie != "T":
@@ -106,25 +112,40 @@ while True:
             except ValueError:
                 print("Nie podano liczby!")
 
-        if wybor_dzialania == '6':
-            print("Pierwiastek kwadratowy liczby", liczba3, "=", pierwiastek_2_liczby(liczba3))
+        if wybor_dzialania == "6":
+            wynik = pierwiastek_2_liczby(liczba3)
+            print("Pierwiastek kwadratowy liczby", liczba3, "=", wynik)
 
-        elif wybor_dzialania == '7':
-            print("Logarytm naturalny liczby", liczba3, "=", logarytmn_nturalny(liczba3))
+        elif wybor_dzialania == "7":
+            wynik = logarytmn_nturalny(liczba3)
+            print("Logarytm naturalny liczby", liczba3, "=", wynik)
 
-        elif wybor_dzialania == '8':
-            print("Logarytm dziesiętny liczby", liczba3, "=", logarytm_10(liczba3))
+        elif wybor_dzialania == "8":
+            wynik = logarytm_10(liczba3)
+            print("Logarytm dziesiętny liczby", liczba3, "=", wynik)
 
-        elif wybor_dzialania == '9':
-            print("Sinus liczby", liczba3, "=", sinus(liczba3))
+        elif wybor_dzialania == "9":
+            wynik = sinus(liczba3)
+            print("Sinus liczby", liczba3, "=", wynik)
 
-        elif wybor_dzialania == '10':
-            print("Tangens liczby", liczba3, "=", tangens(liczba3))
+        elif wybor_dzialania == "10":
+            wynik = tangens(liczba3)
+            print("Tangens liczby", liczba3, "=", wynik)
+
+        znaczek2 = slownik_rd[int(wybor_dzialania)]
+        ostatnie_obliczenie2 = f"{znaczek2} liczby {liczba3} = {wynik}"
+        historia_dzialan(ostatnie_obliczenie2)
 
         nastepne_dzialanie = input("\nCzy chcesz wykonać następne działanie? (T/N): ").upper()
         if nastepne_dzialanie != "T":
             print("Nie wyrażono chęci kontynuowania")
             break
+
+    elif wybor_dzialania == "11":
+            print("\nPoprzednie operacje")
+            historia()
+            print()
+
     else:
         print("Zła liczba/nie ma takiego działania")
 
